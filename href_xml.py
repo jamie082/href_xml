03c2728b0dd1abe978e6edfbf075786bc2236c4b
jamie082 <jamiemorrissey02@gmail.com>
2022-05-22 16:45:58 -0700

    updated html parser

--- href_xml.py
+++ href_xml.py
@@ -1,13 +1,14 @@
 #/usr/bin/python3
 
 # http://www.compjour.org/warmups/govt-text-releases/intro-to-bs4-lxml-parsing-wh-press-briefings/
+# https://www.tutorialspoint.com/beautiful_soup/beautiful_soup_installation.htm
 
 import requests
 from bs4 import BeautifulSoup
 
 url = 'http://sandiego.craigslist.org/search/sof'
-resp = requests.get(url)
-soup = BeautifulSoup(resp.text, 'lxml')
+webpage = url.content
+soup = BeautifulSoup(webpage.text, 'html.parser')
 
 urls = []
 for h in soup.find_all('li'):
