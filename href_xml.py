4c21d6163a6258bfcc3c5ab5dacfa403e03b0812
jamie082 <jamiemorrissey02@gmail.com>
2022-05-22 20:14:24 -0700

    added code for pandas DataFrame

--- href_xml.py
+++ href_xml.py
@@ -1,12 +1,28 @@
 #/usr/bin/python3
 
+# stackabuse/parsing-xml-with-beautifulsoup-in-python/
+
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 url = 'http://sandiego.craigslist.org/search/sof'
 req =  requests.get(url)
 soup = BeautifulSoup(req.text, 'html.parser')
 
+# create DataFrame
+
+df = pd.DataFrame(columns['Description'])
+
 urls = []
 for h in soup.find_all('a'):
     print(h.get('href'))
+
+    row = {
+        'columns': h,
+    }
+
+    df = df.append(row, ignore_index=True)
+    print(f'Appending row %s of %s' % (index+1, items_length))
+
+print(df)
