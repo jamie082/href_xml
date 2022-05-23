2dbb09207c3d8656aade2291cd96b645e6e3f8ea
jamie082 <jamiemorrissey02@gmail.com>
2022-05-22 18:33:25 -0700

    updated source

--- href_xml.py
+++ href_xml.py
@@ -1,8 +1,5 @@
 #/usr/bin/python3
 
-# http://www.compjour.org/warmups/govt-text-releases/intro-to-bs4-lxml-parsing-wh-press-briefings/
-# https://www.tutorialspoint.com/beautiful_soup/beautiful_soup_installation.htm
-
 import requests
 from bs4 import BeautifulSoup
 
