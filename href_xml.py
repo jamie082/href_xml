f0288f14f6dd5f1a56dd2ddfc13bdb56e9b11c36
jamie082 <jamiemorrissey02@gmail.com>
2022-05-26 00:17:01 -0700

    added pprint() function

--- href_xml.py
+++ href_xml.py
@@ -7,7 +7,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
+from pprint import pprint
 
 df = pd.DataFrame(columns=['result-hood', 'datetime']) # create columns
 
@@ -18,15 +18,18 @@ data = requests.get(my_site)
 html = BeautifulSoup(data.text, 'html.parser')
 items = html.select('li')
 
+my_data = [] # create my_data[] for dataFrame
+
 for item in items: # create element ordered list
-    print(items)
-    #href_output = item.find('href')
-    """
-    location_obj = item.find('result-hood')
-    time_obj = item.find('datetime')
+    # new objectss
+    result_hood = item.select('result-hood')
+    item = item.select('datetime')
 
-    urls = []
+    my_data.append({"title": result_hood, "item": item})
 
+pprint(my_data)
+
+"""
     row = {
         #'columns': href_output,
         'href': location_obj,
@@ -36,7 +39,6 @@ for item in items: # create element ordered list
     df = pd.DataFrame(row, index=[0, 1, 2, 3])
 
 print(df)
+
 """
-
-
 # output a href list into Description, Location and Time columns (html.parser)
